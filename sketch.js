e37c6fe989456b462478a1a821737e4a2b8ee899
stephanieparra <stephanieparrastudio@gmail.com>
2025-08-19 17:56:07 -0700

    background and agave img added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 let majorMono;
+let bgImg;
+let img;
 function preload() {
     majorMono = loadFont('fonts/majormono.ttf');
+    img = loadImage("images/agave-nb.png");
+    bgImg = loadImage("images/navy-linen.png");
 }
 
 function setup() {
-    createCanvas(1000, 1000);
+    createCanvas(1000, 600);
+    img.resize(300, 0);
     textFont(majorMono);
     textAlign(LEFT, TOP);
 }
@@ -12,9 +17,8 @@ function setup() {
 let quote = "To remember the future\nis a radical act.\nTo build it with\nancestral memory\nis our technology.";
 
 function draw() {
-    background('#f1f1f1');
-    fill(0);
-
+    image(bgImg, 0, 0, width, height);
+    fill('#f0e5d8');
     let fontSize = 50;
     let lineHeight = 2;
     textSize(fontSize);
@@ -29,7 +33,7 @@ function draw() {
     for (let i = 0; i < lines.length; i++) {
         if (i === 0) {
             // Line 1 is fixed
-            text(lines[i], 50, 0);
+            text(lines[i], 50, 30);
         } else {
             let t = (sin(frameCount * speed) + 1) / 2;
             let offset = amplitude * (1 - t);
@@ -37,4 +41,7 @@ function draw() {
             prevOffsets += offset;
         }
     }
+
+    imageMode(CORNER);
+    image(img, width - img.width - 10, height - img.height - 1);
 }
